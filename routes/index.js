2b791cd6d33937231b343f348960c6222a09d7bc
Mateusz Cyburt <mateusz.cyburt@gmail.com>
2016-01-05 14:20:44 +0100

    added goodguy

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request-promise');
+var goodGuyLib = require('good-guy-http');
+var goodGuy = goodGuyLib({
+    maxRetries: 2,
+    json: true,
+    cache: goodGuyLib.inMemoryCache(10),
+    defaultCaching: {                  // default caching settings for responses without Cache-Control
+        cached: true,                    // - whether such responses should be cached at all
+        timeToLive: 5000,                // - for how many ms
+        mustRevalidate: false            // - is it OK to return a stale response and fetch in the background?
+    }
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  request('https://book-catalog-proxy-5.herokuapp.com/book?isbn=0596805527')
-      .then(function(body) {
-        var result = JSON.parse(body);
-        res.render('index', { title: 'Express', items: result.items });
+router.get('/', function(req, res) {
+    goodGuy('https://book-catalog-proxy-4.herokuapp.com/book?isbn=0596805527')
+      .then(function(result) {
+        res.render('index', { title: 'Express', items: result.body.items });
       })
       .catch(function (err) {
         console.error(err);
