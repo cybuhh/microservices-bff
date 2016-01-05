b9c7e80b8ed2bbe505f47fde58c173dd5569868e
Mateusz Cyburt <mateusz.cyburt@gmail.com>
2016-01-05 13:55:58 +0100

    fetch data from api and render in html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request('https://book-catalog-proxy-5.herokuapp.com/book?isbn=0596805527')
+      .then(function(body) {
+        var result = JSON.parse(body);
+        res.render('index', { title: 'Express', items: result.items });
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
 });
 
 module.exports = router;
