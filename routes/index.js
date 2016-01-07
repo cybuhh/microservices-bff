87dacd688e56bf0c2da128961e19c4804aa795f6
Mateusz Cyburt <mateusz.cyburt@gmail.com>
2016-01-07 10:24:01 +0100

    asdad ads

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.get('/', function(req, res) {
       .then(function(html) {
           return new esi().process(html, {
               headers: {
-                  'custom-request-id': reqId
+                  'x-request-id': reqId
               }
           });
       })
